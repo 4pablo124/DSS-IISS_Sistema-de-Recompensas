dd127cb512aac6a8ea6fe8a40e669fdfbac86201
gittheroadjack <ale_romero97@hotmail.com>
2019-01-09 17:51:25 +0100

    Añadiendo route de videojuego para bd

---
+++ ProyectoRecompensa/routes/videojuego.route.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const videojuegoRoutes = express.Router();
+
+let videojuegoModel = require('..models/videojuego');
+
+videojuegoRoutes.route('/add').post(function(req, res) {
+    let videojuego = new videojuegoModel(req.body);
+    videojuego.save()
+        .then(game => {
+            res.status(200).json({'videojuego': 'videojuego se ha añadido correctamente'});
+        })
+
+        .catch(err => {
+            res.status(400).send("Imposible guardar en la base de datos");
+        })
+});
+
+module.exports = videojuegoRoutes;
