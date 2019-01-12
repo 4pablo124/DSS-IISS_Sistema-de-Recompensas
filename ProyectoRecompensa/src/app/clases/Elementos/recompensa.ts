4e1ac69b829557558e4d2f0a7b2e295d60159bd7
4pablo124 <4pablo125@gmail.com>
2019-01-12 16:55:04 +0100

    añadido id a recompensa

--- ProyectoRecompensa/src/app/clases/Elementos/recompensa.ts
+++ ProyectoRecompensa/src/app/clases/Elementos/recompensa.ts
@@ -2,6 +2,7 @@ import { Dificultad } from './enumerados';
 
 export class Recompensa
 {
+    private id: string;
     constructor(
         private _titulo: string,
         private _descripcion: string,
@@ -10,12 +11,13 @@ export class Recompensa
     )
     {}
 
+    get _id(): string {return this.id;}
     get titulo(): string {return this._titulo;}
     get descripcion(): string {return this._descripcion;}
     get dificultad(): Dificultad {return this._dificultad;}
     get videojuego(): String {return this._videojuego;}
 
-    set titulo(titulo: string) {this.titulo = titulo;}
+    set _id(id: string) {this.id = id;}
     set descripcion(descripcion: string) {this._descripcion = descripcion;}
     set dificultad(dificultad: Dificultad) {this._dificultad = dificultad;}
     set videojuego(videojuego: String) {this._videojuego = videojuego;}
