c0f7b7984958795331a0a4eef6999a17837e0679
gittheroadjack <ale_romero97@hotmail.com>
2019-01-12 22:11:22 +0100

    Preparación de jugador para funcionar con BD

--- ProyectoRecompensa/src/app/menu/forms/jugador/r-jugador-form/r-jugador-form.component.ts
+++ ProyectoRecompensa/src/app/menu/forms/jugador/r-jugador-form/r-jugador-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { iMenu } from 'src/app/clases/Menus/iMenu';
 import { MenuService } from 'src/app/services/menu.service';
+import { JugadorService } from 'src/app/services/BD/jugador.service';
 
 @Component({
   selector: 'app-r-jugador-form',
@@ -9,37 +10,17 @@ import { MenuService } from 'src/app/services/menu.service';
 })
 export class RJugadorFormComponent implements OnInit {
   
+  constructor(private menuService: MenuService, private bd: JugadorService) { }
+
   menu: iMenu;
 
-  jugadores = [
-    {
-      nombre: 'pepe',
-      apellidos: 'flautas',
-      nickname: '123user',
-      email: 'el_mejor@email.ru',
-      fechaNacimiento: Date
-    },
-    {
-      nombre: 'jose',
-      apellidos: 'pacheco',
-      nickname: 'coolGuy_84',
-      email: 'el_peor@email.jp',
-      fechaNacimiento: Date
-    },
-    {
-      nombre: 'anonimo',
-      apellidos: 'gonzalez',
-      nickname: 'mario_Jump',
-      email: 'yahoo@email.org',
-      fechaNacimiento: Date
-    }
-  ];
+  jugadores: any[];
 
-  data = {nombre: '', apellidos: '', nickname: '', email: '', fechaNacimiento: Date};
+  data = {_id: '', nombre: '', apellidos: '', nickname: '', email: '', fechaNacimiento: Date};
 
-  constructor(private menuService: MenuService) { }
 
   ngOnInit() {
     this.menu = this.menuService.selectedMenu;
+    this.bd.findAll().subscribe(jugadores => this.jugadores = jugadores);
   }
 }
