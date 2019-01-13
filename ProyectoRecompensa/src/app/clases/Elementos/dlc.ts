1a483a50ddee3990463cbd9a6eeb967fffe84ba1
4pablo124 <4pablo125@gmail.com>
2019-01-13 13:36:46 +0100

    el atributo videojuego ahora es tipo string (id)

--- ProyectoRecompensa/src/app/clases/Elementos/dlc.ts
+++ ProyectoRecompensa/src/app/clases/Elementos/dlc.ts
@@ -4,16 +4,16 @@ export class DLC
     constructor(
         private _titulo: string,
         private _descripcion: string,
-        private _videojuego: any
+        private _videojuego: string
     ){}
 
     get _id(): string {return this.id;}
     get titulo(): string {return this._titulo;}
     get descripcion(): string {return this._descripcion;}
-    get videojuego(): any {return this._videojuego;}
+    get videojuego(): string {return this._videojuego;}
 
     set _id(id: string) {this.id = id;}
     set titulo(titulo: string) {this._titulo = titulo;}
     set descripcion(descripcion: string) {this._descripcion = descripcion;}
-    set videojuego(videojuego: any) {this._videojuego = videojuego;}
+    set videojuego(videojuego: string) {this._videojuego = videojuego;}
 }
